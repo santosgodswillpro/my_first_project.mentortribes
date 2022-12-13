288844bf2637e0c7abd997f315b4fa3671b50dde
Santos Godswill <santosgodswill16@gmail.com>
2022-12-13 18:18:03 +0100

    just finish working on fonts

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div class="overall">
+      <main class="container">
+        <div class="head-container">
+          <div class="img-contaner">
+            <img src="/image-qr-code.png" alt="" />
+          </div>
+          <h2>Improve your frontend skills by building projects</h2>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
